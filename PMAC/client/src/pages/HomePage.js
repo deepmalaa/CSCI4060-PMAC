41931d6d9d45a84ae78bbb71100cc7c6b51c159d
BitterFrostbite <bryanedwards3@yahoo.com>
2023-03-18 12:25:12 -0500

    Student Landing Alpha Mock Up

--- PMAC/client/src/pages/HomePage.js
+++ PMAC/client/src/pages/HomePage.js
@@ -15,9 +15,9 @@ function FormOne() {
                     <div className={s.whiteBar}>
                         <ul>
                             <li><a href="#home">Home</a></li>
-                            <li><a href="#news">News</a></li>
+                            <li><a href="#news">Create Account</a></li>
                             <li><a href="#contact">Contact</a></li>
-                            <li><a href="#about">About</a></li>
+                            
                             <li><a href="#Help">Help</a></li>
                         </ul>
 

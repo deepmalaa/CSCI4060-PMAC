ecc4cbd4b3efac849af9c01fccb3e849dfe613da
hendrixj <madihendrix@icloud.com>
2023-04-10 19:20:59 -0500

    Pulling info from user DB for status(Not finished)

--- PMAC/client/src/pages/StatusPage.js
+++ PMAC/client/src/pages/StatusPage.js
@@ -3,29 +3,58 @@ import React, { useState, useEffect } from 'react';
 import {connect} from 'react-redux';
 import {Link, Navigate} from 'react-router-dom';
 import ApplicationSelector from './ApplicationSelector';
+import user1 from './ApplicantInformation';
 import '../styles/StatusPage.css';
 import Details from './StatusPageDetails';
 import s from '../styles/HomePage.module.css';
 import topBanner from '../img/StatusPage.jpg';
 import { getCurrentProfile } from '../actions/profile';
 import { applicantRelease } from '../actions/applicantRelease';
+import { ApplicantInfo } from '../actions/applicantInformation';
 import PropTypes from 'prop-types';
 import Sidebar from '../components/layout/Sidebar';
 
 
+
 const StatusPage =({getCurrentProfile, applicantRelease, auth: { user }, profile: { profile }}) =>{
   useEffect(() => {getCurrentProfile();}, [getCurrentProfile]);
-
+  
   const [selectedApplication, setSelectedApplication] = useState('');
   const [status, setStatus] = useState('');
   const [showApplicationSelector, setShowApplicationSelector] = useState(true); // new state variable
+
+  // Users full name
+  const Fullname = profile.fname + " " + profile.mname + " " + profile.lname;
   
-  const UserName = user != null ? user.name : "";
+  // Default verified false
+  let Verified1 = false;
+  let Verified2 = false;
+  let Verified3 = false;
+  let Verified4 = false;
+  let Verified5 = false;
+ 
+  // Checks to see what user is applying for
+  if(profile.amcas_id !== '' && profile.amcas_id !== '') {
+    Verified1 = true;  
+  }
+  if(profile.aacomas_id !== null && profile.aacomas_id !== '') {
+    Verified2 = true;  
+  }
+  if(profile.aadsas_id !== null && profile.aadsas_id !== '') {
+    Verified3 = true;  
+  }
+  if(profile.aamc_id_id != null && profile.aamc_id !== '') {
+    Verified4 = true;  
+  }
+  if(profile.caspa_id != null && profile.caspa_id !== '') {
+    Verified5 = true;  
+  }
+
 
     const submittedApplications = {
       application1: {
-        name: UserName,
-        verified: true,
+        name: Fullname,
+        verified: Verified1,
         title: "Medical Application",
         status: 'Pending',
         interviewStatus: false,
@@ -33,32 +62,32 @@ const StatusPage =({getCurrentProfile, applicantRelease, auth: { user }, profile
       },
 
       application2: {
-        name: UserName,
-        verified: true,
+        name: Fullname,
+        verified: Verified2,
         title: 'Osteopathic Medical Application',
         status: 'Accepted',
         interviewStatus: false,
         submissionDate: 'month/day/year',
       },
       application3: {
-        name: UserName,
-        verified: true,
+        name: Fullname,
+        verified: Verified3,
         title: 'Physician Assistant Application',
         status: 'Denied',
         interviewStatus: false,
         submissionDate: 'month/day/year',
       },
       application4: {
-        name: UserName,
-        verified: true,
+        name: Fullname,
+        verified: Verified4,
         title: 'Dental Application',
         status: 'Interview',
         interviewStatus: false,
         submissionDate: 'month/day/year',
       },
       application5: {
-        name: UserName,
-        verified: true,
+        name: Fullname,
+        verified: Verified5,
         title: 'Other(ex: Podiatry) Application',
         status: 'Complete',
         interviewStatus: false,
@@ -94,7 +123,7 @@ const StatusPage =({getCurrentProfile, applicantRelease, auth: { user }, profile
         </div> 
 
         <div>
-            <div className={s.whiteBar}>
+            <div className={s.whiteBar} style={{marginTop:'-6px'}}>
             <div className={s.goldBars}> </div>
                 <ul>
                 <li><a href="#Home">Home</a></li>

f38b33a207c6722a7b4cf4d1573ee99153426f63
BitterFrostbite <bryanedwards3@yahoo.com>
2023-04-18 22:50:55 -0500

    Testing profile save

--- PMAC/routes/api/profile.js
+++ PMAC/routes/api/profile.js
@@ -38,7 +38,27 @@ router.get('/me', auth, async (req, res) => {
 // @access  Private
 router.post('/', [auth, [
   check('fname', 'Name is required').not().isEmpty(),
-  check('ulm_email', 'Please include a valid email').isEmail(),
+  check('ulm_email', 'Please include a valid ULM email').isEmail(),
+  check('lname', 'Please include a valid last name').notEmpty(),
+  check('cwid', 'Please include a valid cwid').not().isEmpty(),
+  check('address', 'Please include a valid address').not().isEmpty(),
+  check('alt_email', 'Please include a valid alternative email').isEmail(),
+  check('major', 'Please include a valid major').not().isEmpty(),
+  check('grad_date', 'Please include a graduation date').not().isEmpty(),
+  check('gpa', 'Please include a valid gpa').not().isEmpty(),
+  check('entrance_date', 'Please include a valid entrance date').not().isEmpty(),
+  check('mcat', 'Please include a valid mcat').not().isEmpty(),
+  check('dat', 'Please include a valid dat').not().isEmpty(),
+  check('oat', 'Please include a valid oat').not().isEmpty(),
+  check('gre', 'Please include a valid gre').not().isEmpty(),
+  check('schoolType', 'Please include a valid school type').not().isEmpty(),
+  check('exam_date', 'Please include a valid exam date').not().isEmpty(),
+  check('amcas_id', 'Please include a valid amcas_id').not().isEmpty(),
+  check('aacomas_id', 'Please include a valid aacomas_id').not().isEmpty(),
+  check('aadsas_id', 'Please include a valid aadsas_id').not().isEmpty(),
+  check('aamc_id', 'Please include a valid aamc_id').not().isEmpty(),
+  check('caspa_id', 'Please include a valid caspa_id').not().isEmpty(),
+  check('facultyEval', 'Please include valid faculty names').not().isEmpty(),
 ]], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -134,6 +154,104 @@ router.post('/', [auth, [
 
 });
 
+
+
+// FOR SAVING
+router.post('/save', [auth, [
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    fname,
+    lname,
+    mname,
+    cwid,
+    address,
+    cell,
+    ulm_email,
+    alt_email,
+    bdate,
+    major,
+    minor,
+    grad_date,
+    gpa,
+    entrance_date,
+    mcat,
+    dat,
+    oat,
+    gre,
+    scoreBreakdown,
+    schoolType,
+    exam_date,
+    amcas_id,
+    aacomas_id,
+    aadsas_id,
+    aamc_id,
+    caspa_id,
+    facultyEval,
+
+
+  } = req.body;
+
+  //build profile obj
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (fname) profileFields.fname = fname;
+  if (lname) profileFields.lname = lname;
+  if (mname) profileFields.mname = mname;
+  if (cwid) profileFields.cwid = cwid;
+  if (address) profileFields.address = address;
+  if (cell) profileFields.cell = cell;
+  if (ulm_email) profileFields.ulm_email = ulm_email;
+  if (alt_email) profileFields.alt_email = alt_email;
+  if (bdate) profileFields.bdate = bdate;
+  if (major) profileFields.major = major;
+  if (minor) profileFields.minor = minor;
+  if (grad_date) profileFields.grad_date = grad_date;
+  if (gpa) profileFields.gpa = gpa;
+  if (entrance_date) profileFields.entrance_date = entrance_date;
+  if (mcat) profileFields.mcat = mcat;
+  if (dat) profileFields.dat = dat;
+  if (oat) profileFields.oat = oat;
+  if (gre) profileFields.gre = gre;
+  if (scoreBreakdown) profileFields.scoreBreakdown = scoreBreakdown;
+  if (schoolType) profileFields.schoolType = schoolType;
+  if (exam_date) profileFields.exam_date = exam_date;
+  if (amcas_id) profileFields.amcas_id = amcas_id;
+  if (aacomas_id) profileFields.aacomas_id = aacomas_id;
+  if (aadsas_id) profileFields.aadsas_id = aadsas_id;
+  if (aamc_id) profileFields.aamc_id = aamc_id;
+  if (caspa_id) profileFields.caspa_id = caspa_id;
+  if (facultyEval) profileFields.facultyEval = facultyEval;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    if (profile) {
+      //update
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true },
+      );
+      return res.json(profile);
+    }
+
+
+    //Create
+    profile = new Profile(profileFields);
+    await profile.save();
+    res.json(profile);
+  }
+  catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
+});
 // // @route    PUT api/profile/work-experience
 // // @desc     Add profile experience
 // // @access   Private

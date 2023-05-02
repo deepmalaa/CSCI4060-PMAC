2af32878607992c2e970b0f6f13c45b901edf21e
hendrixj <madihendrix@icloud.com>
2023-05-01 23:17:38 -0500

    Cosmetics...again

--- PMAC/client/src/pages/EvaluationPage.js
+++ PMAC/client/src/pages/EvaluationPage.js
@@ -7,7 +7,7 @@ function EvaluationPage(props) {
   const { myProp, profile, evaluator, applicationType } = props;
 
   return (
-    <form className={s.form} style={{height:'100%'}}>
+    <form className={s.form} style={{height:'100%', minHeight:'400px'}}>
       
       <div style={{ textAlign: 'left', maxWidth: '100%', margin:'10px'}} dangerouslySetInnerHTML={{ __html: myProp }} />
     </form>
